175bff45910e291ea578947b03dcf780a12f4988
Mac Air <macair@MacBook-Air-cua-Mac.local>
2026-05-26 22:10:05 +0700

    Doi ten the title va favicon

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
         "Xử lý kỵ khí: Bổ sung cacbon cho vi sinh vật yếm khí phân hủy chất hữu cơ.",
         "Bổ sung dinh dưỡng: Cung cấp dưỡng chất thiếu hụt trong nước thải để nâng cao hiệu quả xử lý.",
         "Pha loãng với nước (tỷ lệ 1:10 hoặc 1:20).Đóng vai trò là nguồn dinh dưỡng carbon thiết yếu để vi sinh vật (EM1, phân hữu cơ) phát triển mạnh mẽ.",
-        
       ]
     },
     {
